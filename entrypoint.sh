68f027896c46b646b371263a9957bac8932ae902
koori69 <koori69@users.noreply.github.com>
2018-07-24 15:12:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if [ "$SS_MODULE" == "" ]; then
-    echo -e "SS_MODULE is null, use ss-server for default."
-    SS_MODULE="ss-server"
-else
-    echo -e "cmd use $SS_MODULE"
-fi
-
 if [ "$SS_METHOD" == "" ]; then
     echo -e "SS_METHOD is null, use aes-256-cfb for default."
     SS_METHOD="aes-256-cfb"
@@ -19,4 +12,4 @@ if [ "$SS_PWD" == "" ]; then
     exit 1
 fi
 
-$SS_MODULE -s 0.0.0.0 -p $PORT -k $SS_PWD -m $SS_METHOD
+ssserver -p $PORT -k $SS_PWD -m $SS_METHOD
