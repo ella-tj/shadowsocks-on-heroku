cc300fb91d2f1d7004d4a5954b6e73ec0f9403dc
koori69 <koori69@users.noreply.github.com>
2018-07-25 10:19:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,4 +13,12 @@ if [ "$SS_PWD" == "" ]; then
 fi
 
 # ssserver -p $PORT -k $SS_PWD -m $SS_METHOD
+echo { > /etc/shadowsocks-libev/config.json
+echo "server":"127.0.0.1", >> /etc/shadowsocks-libev/config.json
+echo "server_port":$PORT, >> /etc/shadowsocks-libev/config.json
+echo "local_port":1080, >> /etc/shadowsocks-libev/config.json
+echo "password":"$SS_PWD", >> /etc/shadowsocks-libev/config.json
+echo "timeout":60, >> /etc/shadowsocks-libev/config.json
+echo "method":"$SS_METHOD", >> /etc/shadowsocks-libev/config.json
+
 /etc/init.d/shadowsocks-libev start 
